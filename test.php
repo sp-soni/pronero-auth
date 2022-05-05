9398caec86cba871a81fe0284ab3a22d19a0b0fb
SPSONI <sheetala.prasadsoni@pressganey.com>
2022-05-05 15:42:44 +0530

    commit

---
+++ test.php
@@ -0,0 +1,18 @@
+//SpiTech API & PDF
+require_once('../php/SpiTechApi/dompdf/SpiTechPdf.php');
+require_once('../php/SpiTechApi/spitech/SpiTechApi.php');
+
+//SpiTechPDF
+$config = [
+    'base_path' => 'D:\wamp64\www\spsoni\billing.spitech.in\\'
+];
+$pdf = new SpiTechPdf($config);
+$pdf->generatePDF("Menu/demo.html", "demp.pdf", 1);
+
+//SpiTechApi
+$params = array(
+    "logPath" => "../logs/",
+    'cookiePath' => COOKIE_PATH,  // this constant is coming from Conn.php file
+    'apiKey' => '61w5W4Rp0ZcB9k9i9A5smx0b11gzP1'
+);
+$objSpiTechApi = SpiTechApi::getInstance($params);
