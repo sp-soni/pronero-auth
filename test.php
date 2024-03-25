6c96cb71f72556ea7ebbd8afebf02aebf0a9cb52
Anand <anand@goworkstream.io>
2024-03-25 11:41:36 +0530

    pronero api setup done

--- test.php
+++ test.php
@@ -1,34 +1,16 @@
 <?php
-define('COOKIE_PATH', '/spsoni/spitech-php-libraries/');
-function debug($args, $die = 1)
-{
-    echo "<pre>";
-    print_r($args);
-    echo "</pre>";
-}
 
+require_once 'src/Pronero.php';
 
+$pronero = new Pronero('c081547e59a076e978ea9a6bac147a0c');
 
-//SpiTech API & PDF
-require_once('SpiTechApi/dompdf/SpiTechPdf.php');
-require_once('SpiTechApi/spitech/SpiTechApi.php');
+//projectInfo
+$apiResponse1 = $pronero->projectInfo();
 
-//SpiTechPDF
-$config = [
-    'base_path' => 'D:\wamp64\www\spsoni\billing.spitech.in\\',
-    'pdf_dir' => ''
-];
-$pdf = new SpiTechPdf($config);
-//$pdf->generatePDF("Menu/demo.html", "demp.pdf", 1);
+//login
+//$apiResponse2 = $pronero->login('superadmin@pronero.in','Meta@190712');
 
-//SpiTechApi
-$params = array(
-    "logPath" => "../logs/",
-    'cookiePath' => COOKIE_PATH,  // this constant is coming from Conn.php file
-    'apiKey' => '61w5W4Rp0ZcB9k9i9A5smx0b11gzP1'
-);
-$objSpiTechApi = SpiTechApi::getInstance($params);
+//forgotPassword
+// $apiResponse3 = $pronero->forgotPassword('superadmin@pronero.in');
 
-
-$res = $objSpiTechApi->sendMail('spitechsoft@gmail.com', "Test from SpiTech PHP Library", "Demo Message" . __DIR__);
-debug($res);
+$pronero->debug($apiResponse1);
